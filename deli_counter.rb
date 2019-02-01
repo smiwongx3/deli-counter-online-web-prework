ca219dd68ec9d4a6ecd39f324d4cb1b01cf81019
Samantha Wong <smiwong920@gmail.com>
2019-02-01 21:47:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,14 @@ def line(katz_deli)
   end
 end
 
+def line(deli)
+  if deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
+end

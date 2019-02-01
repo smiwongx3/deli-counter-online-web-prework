d86bfba3c69eaec84b23d631ab82680fdbaaa0a8
Samantha Wong <smiwong920@gmail.com>
2019-02-01 21:49:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,13 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length > 0 
-    katz_deli.each_with_index.map do |name, index|
-    
+  if katz_deli.empty? 
+    puts "The line is currently empty."
   else 
-    puts "The line is currently empty." 
+    current_line = "The line is currently:"
+    katz_deli.each_with_index(1) do |person, i|
+      current_line << "#{i}. #{person}"
   end
+  puts current_line
 end
 
-def line(deli)
-  if deli.empty?
-    puts "The line is currently empty."
-  else
-    current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
-      current_line << " #{i}. #{person}"
-    end
-    puts current_line
-  end
-end
